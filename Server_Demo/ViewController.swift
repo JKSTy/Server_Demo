03f1c8c748a834a08a4c7dad1637425632f2ac56
sunil <sunil@codecraft.co.in>
2017-03-15 10:00:34 +0530

    first commit

---
+++ Server_Demo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Server_Demo
+//
+//  Created by Jaswanth on 15/03/17.
+//  Copyright © 2017 CodeCraft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.initialSettings()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+
+    }
+
+    func initialSettings() {
+        
+    }
+}
+
